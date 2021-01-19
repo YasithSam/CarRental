ffe47ad64ac87c83ff679b915d2150fed1048c3a
YasithSam <yasith.sam7@gmail.com>
2021-01-19 20:41:34 +0530

    Dashboard Feautures added

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -6,6 +6,7 @@
 <link href='https://css.gg/community.css' rel='stylesheet'>
 <link href='https://css.gg/pentagon-down.css' rel='stylesheet'>
 <link href='https://css.gg/user.css' rel='stylesheet'>
+<link rel="stylesheet" href="../style.css"/>
 <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 <body>
         <?php include('../db/connection.php');?> 
@@ -15,20 +16,14 @@
             ---- App Header 
             --->
             
-            <div class="app-header">
-                <div class="app-branding">
-                    <img src="../Assets/4891.jpg" width="50px;" height="50px;">
-                    <h1 class="app-brand" style="padding-left: 5px;">Hire Me</h1>
-                </div>
-                <div class="app-nav">
-                    <p>Navigation</p>
-                </div>
-            </div>
+           <?php include ('./common.php');?>
             
             <!-- 
             ---- App Content 
             --->
             <div class="container-fluid">
+              <h1 style="margin: 30px 0px 20px 10px; font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif">Admin Panel</h1>
+              <hr class="hr-1">
               <div class="row" style="margin: 100px 10px 0px 5px;">
                
                 <div class="column">
@@ -49,27 +44,54 @@
                 </div>
 
                 <div class="column">
-                    <div class="card">
-                        <h3>Card 2</h3>
-                        <p>Some text</p>
-                        <p>Some text</p>
-                    </div>
+                  <div class="serviceBox">
+                     
+                        <?php 
+                            $sql = "SELECT COUNT(*) AS Count FROM users";
+                            $result = $con->query($sql);
+                            $row = $result->fetch_assoc();
+                            
+                        ?>
+                        
+                            <h3 class="count"><?php  echo $row['Count'];?></h3>
+                            <h4><p class="title">Registered Users</p>
+                            <button class="btn card_btn">Read More</button>
+                       
+                  </div>    
                 </div>
                 
                 <div class="column">
-                    <div class="card">
-                        <h3>Card 3</h3>
-                        <p>Some text</p>
-                        <p>Some text</p>
-                    </div>
+                  <div class="serviceBox">
+                     
+                        <?php 
+                            $sql = "SELECT COUNT(*) AS Count FROM users";
+                            $result = $con->query($sql);
+                            $row = $result->fetch_assoc();
+                            
+                        ?>
+                        
+                            <h3 class="count"><?php  echo $row['Count'];?></h3>
+                            <h4><p class="title">Registered Users</p>
+                            <button class="btn card_btn">Read More</button>
+                       
+                  </div>    
                 </div>
                 
                 <div class="column">
-                    <div class="card">
-                        <h3>Card 4</h3>
-                        <p>Some text</p>
-                        <p>Some text</p>
-                    </div>
+                  <div class="serviceBox">
+                     
+                        <?php 
+                            $sql = "SELECT COUNT(*) AS Count FROM users";
+                            $result = $con->query($sql);
+                            $row = $result->fetch_assoc();
+                            
+                        ?>
+                        
+                            <h3 class="count"><?php  echo $row['Count'];?></h3>
+                            <h4><p class="title">Registered Users</p>
+                            <button class="btn card_btn">Read More</button>
+                       
+                  </div>    
                 </div>
              </div>
             </div> 
@@ -77,43 +99,10 @@
             <!-- 
             ---- App Sidebar
             --->
-            <div class="app-sidebar">
-
-                <ul class="app-sidebar-menu">
-                    <li class="active" id="item" style="padding-left: 25px;">
-                        <a href="#dashboard">
-                        <span class="gg-menu-grid-o" id="icon"></span>
-                        <span class="list-a" style="padding-left: 5px;">Dashboard</span>
-                        </a>
-                    </li>
-                    <li id="item" style="padding-left: 25px;">
-                        <a href="#serverlogs">
-                        <span class="gg-user" id="icon"></span>
-                        <span class="list-a" style="padding-left: 8px;">Users</span>
-                    </a>
-                    </li>
-                    <li id="item">
-                        <a href="#payment">
-                        <span class="gg-community" id="icon"></span>
-                        <span class="list-a">Brands</span>
-                    </a>
-                    </li>
-                    <li id="item">
-                        <a href="#customers">
-                        <span class="fas fa-car" id="icon"></span>
-                        <span class="list-a">Vehicles</span>
-                    </a>
-                    </li>
-                    <li id="item">
-                        <a href="#serverlogs">
-                        <span class="gg-pentagon-down" id="icon"></span>
-                        <span class="list-a">Bookings</span>
-                    </a>
-                    </li>
-                </ul>
-
-            </div>
+            <?php include('./nav.php');?>
         </div>
+
+        <script src="../js/dashboard.js"></script>
 
 </body>
 </html>
