8feed73fafb3cb1ab59875148c8e1826aabcbd3b
YasithSam <yasith.sam7@gmail.com>
2021-02-01 19:03:56 +0530

    Initial page

---
+++ exit.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+    <title>Initial Page</title>
+    <link rel="stylesheet" href="./css/exit.css">
+</head>
+
+<body>
+
+<div class="header">
+    <!--<img class="logo" src="Assets/4891.jpg" alt="CompanyLogo"> -->
+    <a href="home.php">Hire<span>Me</span></a>
+    <div class="header-right">
+    </div>
+</div>
+
+
+<div class="heading">
+  <div>
+    <p class="slide-up"><span class="red">Hello there,</span></p>
+  </div>
+  <div>
+    <p class="slide-up">Welcome to HireMe.</p>
+  </div>
+  <!--<div>
+    <p class="slide-up">test</p>
+  </div> -->
+</div>
+<br /><br />
+
+<div class="q">
+    <!--<h1 class="q_head">Are you a?</h1> -->
+
+<!-- Sign in Buttons-->
+    <button type="button" action="">Sign up to drive</button>&emsp;
+        &nbsp;<button type="button" action="">Sign up to ride</button>
+        
+</div>
+</body>
+
+</html>
