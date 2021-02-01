892312f16ae53b1011ddc1f4a89b9d58c6a3d1df
YasithSam <yasith.sam7@gmail.com>
2021-02-01 14:52:41 +0530

    Booking is added

---
+++ owner.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body{background-image:url(background.jpg);
+     background-attachment:fixed;
+     background-size:cover;
+     background-position:center; }
+    </style>
+    <meta charset="utf-8"/>
+    <title>Registration</title>
+    <link rel="stylesheet" href="./css/login.css"/>
+</head>
+<body>
+    <form class="form" method="post" action="registration-check.php">
+        <h1 class="login-title" style="color: black;">Registration</h1>
+        <input type="text" class="login-input" name="name" placeholder="User Name" required />
+        <input type="text" class="login-input" name="email" placeholder="Email Adress" required/>
+        <input type="date" class="login-input" name="dob" placeholder="Date of Birth">
+        <input type="number" class="login-input" name="phone" placeholder="Contact Number">
+        <input type="text" class="login-input" name="city" placeholder="City">
+        <input type="password" class="login-input" name="password" placeholder="Password" required />
+        <input type="password" class="login-input" name="confirm_password" placeholder="Confirm Password" required/>
+        <input type="submit" id="button" value="Register" class="login-button">
+        <p class="link"><a href="login.php">Click to Login</a></p>
+    </form>
+
+</body>
+</html>
--- vehicle.php
+++ vehicle.php
@@ -2,11 +2,11 @@
   <div class="column">
      <?php   
            
-          $sql = "SELECT vehicles.vehicle_title,Brand.name,vehicles.vehicles_overview,vehicles.price_per_km,vehicles.city,vehicles.vimage1 FROM vehicles,Brand where Brand.id=vehicles_brand";
+          $sql = "SELECT vehicles.vehicle_title,vehicles.owner_id,vehicles.id,Brand.name,vehicles.seats,vehicles.rating,vehicles.status,vehicles.price_per_km,vehicles.city,vehicles.vimage1 FROM vehicles,Brand where Brand.id=vehicles_brand";
           $result = $con->query($sql);
           if(mysqli_num_rows($result)>0){
                while ($row = mysqli_fetch_assoc($result)){
-                    component($row['vehicle_title'],$row['name'],$row['vehicles_overview'],$row['price_per_km'],$row['city'],$row['vimage1']);
+                    component($row['owner_id'],$row['id'],$row['vehicle_title'],$row['name'],$row['seats'],$row['rating'],$row['status'],$row['price_per_km'],$row['city'],$row['vimage1']);
                }
           }
           else{
