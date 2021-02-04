8103a434e2d91a1a0c0b9bba62c492802d525001
YasithSam <yasith.sam7@gmail.com>
2021-02-04 18:38:21 +0530

    Images added

---
+++ help.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Help</title>
+    <link rel="stylesheet" href="./css/home.css">
+    
+    <link rel="stylesheet" href="./css/help.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+
+
+<?php 
+include("./header.php");
+include("./functions.php");
+?>
+
+<div class="help-head">
+    <br /><br /><br />
+    <h1>Help and Support.</h1>
+    <h4>Home - Help</h4>
+</div>
+
+<div class="help-body">
+    <p class="helpP_head">FREQUENTLY ASKED QUESTIONS AT HIREME RENT A CAR</p>
+    <p class="parai"> The HIREME rent a car FAQs section is an useful source for you to find out answers for any of those questions you may have HIREME the service we provide, whether it's a query about your booking or  Bonus Cards - we have it covered. Please follow the page to find out the answers. </p>
+    <br />
+    <hr>
+</div>
+<br /><br />
+
+<div class="container">
+
+<!--1nd section------------------------------>
+    <div class="panel-group" >
+        <div class="faqHeader">MOST COMMON FAQS</div>
+
+        <div  class="panel-title">
+            <h4>
+                 Is account registration required?
+            </h4>
+        </div>
+            <div class="panel-body">
+                Account registration at <strong>HireMe</strong> is only required if you will be a Driver or Rider. 
+                This ensures a valid Drivers and valid Riders. 
+            </div>
+
+        
+        <div class="panel-title">
+            <h4>What proof of identification(ID)/ document do I need to bring ith me when collecting the vehicle?
+            </h4>
+        </div>
+        <div class="panel-body">
+
+            <ul>
+                <li>A valid driver's license for all named drivers - If your driver’s license is printed in non-Roman alphabet (Arabic, Chinese, Cyrillic, etc.), it must be accompanied by an international driver's license. Any driver's license originating from countries not part of the international driver's license treaty must also be accompanied by an official translation.</li>
+                <li>Renters not from the Sri Lanka must provide a valid government issued secondary form of identification such as a passport.</li>
+                <li>For local renters using a debit card, one utility bill will be required.</li>
+                </ul>
+        </div>
+
+        <div class="panel-title">
+            <h4>What is the minimum age to dreive a rental vehicle?
+            </h4>
+        </div>
+        <div class="panel-body">
+                The minimum age to rent a vehicle in most locations is 21 and the driver must have a valid driver's license. If the driver is between the ages of 21 to 24 only the following categories can be rented – economy vehicle, compact vehicle, intermediate vehicle, full size vehicle, compact SUV, intermediate SUV, and minivan. For an explanation of our vehicle groups, please visit our fleet guide. Please also keep in mind, that an Underage Driver Fee will apply for all drivers under 25 years of age.
+            </div>
+
+        <div class="panel-title">
+            <h4>Can I add a additional driver?
+            </h4>
+        </div>
+        <div class="panel-body">
+            Yes, we do allow up to 10 additional drivers per contract. If the additional drivers cannot be present when you pick up the vehicle, they can be registered with their valid driving license at any other HIREME branch (main driver must be present as well). A fee will be applied for adding each additional driver. If you are Gold, Platinum or Diamond status you can add one additional driver free of charge.        
+        </div>
+    </div>
+
+
+<!--2nd section------------------------------>
+    <br><br><br>
+    <div class="panel-group" >
+        <div class="faqHeader">COVID - 19</div>
+
+        <div  class="panel-title">
+            <h4>
+                CLEANING OF VEHICLE EXTERIOR
+            </h4>
+        </div>
+            <div class="panel-body">
+            <ul>
+                <li>Perform vehicle inspection.</li>
+                <li>Inspect interior.</li>
+            </ul> 
+            </div>
+        </div>
+
+        <div  class="panel-title">
+            <h4>
+                CLEANING OF VEHICLE EXTERIOR
+            </h4>
+        </div>
+            <div class="panel-body">
+                <ul>
+                    <li>Wiper fluid check and replenishment.</li>
+                    <li>Wash exterior of the vehicle.</li>
+                </ul> 
+            </div>
+        </div>
+
+        <div  class="panel-title">
+            <h4>
+                CLEANING OF THE VEHICLE INTERIOR.
+            </h4>
+        </div>
+            <div class="panel-body">
+                <ul>
+                    <li>Remove trash and lost/found items..</li>
+                    <li>Vacuum interior front seat area.</li>
+                    <li>Vacuum interior rear seat area.</li>
+                    <li>Clean window interiors.</li>
+                    <li>Vacuum trunk.</li>
+                </ul> 
+            </div>
+        <div  class="panel-title">
+            <h4>
+                DEEP CLEANING OF COMMON TOUCHPOINTS.
+            </h4>
+        </div>
+            <div class="panel-body">
+                <ul>
+                    <li>Steering wheels.</li>
+                    <li>Dashboard.</li>
+                    <li>Hand grips.</li>
+                    <li>Radio dials.</li>
+                    <li>Cupholders.</li>
+                    <li>Center consoles.</li>
+                    <li>Door handles – inside and outside.</li>
+                </ul> 
+            </div>
+        </div>
+    
+    </div>
+
+    <!--3nd section------------------------------>
+    <br><br><br>
+    <div class="panel-group" >
+        <div class="faqHeader">Billing</div>
+
+        <div  class="panel-title">
+            <h4>
+                 How can I obtain a copy of my invoice/receipt?
+            </h4>
+        </div>
+            <div class="panel-body">
+            At HireMe, you can view and obtain copies of your past rental invoices up to six months from the return of your rental by logging in to your HireME account. For recent returns, please allow up to 3 business days from the date of vehicle return for a copy of the rental receipt to be available. If you require a copy of an invoice older than 6 months, then you may send an email to customerservicehireme@hireme.com to request a copy from our Customer Service specialists. 
+            </div>
+
+        <div class="panel-title">
+            <h4>Why did I not receive the full amount of my Security hold?
+            </h4>
+        </div>
+        <div class="panel-body">
+            If you did not receive the full amount of the security hold, then your rental price did not cover all charges incurred during your rental. When you return your vehicle, a final audit will be conducted to determine whether there are additional charges that were not paid as part of the rental price. An example of such charges may include fuel replacement, a late return (beyond our 30-minute grace period), and/or a special cleaning. In this case, the returned amount of the security hold will be less.
+            </div>
+
+        <div class="panel-title">
+            <h4>Who can I speak to regarding questions about my invoice?
+            </h4>
+        </div>
+        <div class="panel-body">
+            We encourage our customers to contact HireMe Customer Service by email at customerservicehireme@hireme.com and a specialist will be happy to assist!
+        </div>
+    </div>
+
+    <!--4nd section------------------------------>
+    <br><br><br>
+    <div class="panel-group" >
+        <div class="faqHeader">CAR AND VAN VEHICLE INFORMATION.</div>
+        
+        
+        <div class="panel-title">
+            <h4 >Are pets alloed in the car?
+            </h4>
+        </div>
+        <div class="panel-body">
+            Pets are allowed in rental vehicles. Customers must return their rental car in clean condition and free of pet hair to avoid cleaning and detailing fees.
+        </div>
+
+        <div  class="panel-title">
+            <h4>
+                 Is smoking permited in the HireMe rental vehicles?
+            </h4>
+        </div>
+            <div class="panel-body">
+                No, HireMe maintains a 100% smoke-free fleet in the SL and any type of smoking (pipe, cigarette, cigar) is prohibited in all HireME vehicles. Just as renters are assessed a fee for cleaning the car's interior for excessive stains, dirt or soiling attributable to the renter’s use, customers will be assessed an additional charge if the car smells of smoke when it is returned. 
+            </div>
+
+        
+        <div class="panel-title">
+            <h4 >Can I Take the vehicle out of the state of rental?
+            </h4>
+        </div>
+        <div class="panel-body">
+            Territorial restrictions apply depending on the state where the rental takes place.
+        </div>
+
+        <div class="panel-title">
+            <h4>Can vehicle be delivered?
+            </h4>
+        </div>
+        <div class="panel-body">
+            HireMe offers delivery/collection service from specific locations only. For locations that offer this service, please contact us at reservationshireMe@hireme.com.
+        </div>
+    </div>
+
+</div>
+<br><br><br>
+<!-- Footer -->
+<?php include("./footer.php")?>
+</body>
+</html>
