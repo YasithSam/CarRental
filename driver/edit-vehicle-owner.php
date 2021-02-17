40c3848f3bfd4cd8a661faeff9999441a6e9f33b
YasithSam <yasith.sam7@gmail.com>
2021-02-17 22:37:05 +0530

    driver added

---
+++ driver/edit-vehicle-owner.php
@@ -0,0 +1,56 @@
+<?php
+require('../db/connection.php');
+$id=$_REQUEST['id'];
+$query = "SELECT * from vehicles where id='".$id."'"; 
+$result = mysqli_query($con, $query);
+$row = mysqli_fetch_assoc($result);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Update Record</title>
+    <link rel="stylesheet" href="../css/edit.css" />
+</head>
+    <body>
+    <?php
+            $status = "";
+            if(isset($_POST['new']) && $_POST['new']==1)
+            {
+                $id=$_REQUEST['id'];
+                $title =$_REQUEST['title'];
+                $price =$_REQUEST['price'];
+                $city =$_REQUEST['city'];
+                $update="update vehicles set  
+                vehicle_title='".$title."',price_per_km='".$price."',city='".$city."'
+                 where id='".$id."'";
+                mysqli_query($con, $update);
+                $status = "Record Updated Successfully. </br></br>";
+                header("Location: ../profile.php");
+                
+            }else {
+            ?>
+           
+              <form class="form" method="post" action="">
+                    <h1 class="login-title">Update Records</h1>
+                    <input type="hidden" name="new" value="1" />
+                    <input name="id" type="hidden" value="<?php echo $row['id'];?>" />
+                    
+                    <input type="text" class="login-input" name="title" placeholder="Vehicle Title" required value="<?php echo $row['vehicle_title'];?>"/>
+                    <input type="text" class="login-input" name="price" placeholder="Price" required value="<?php echo $row['price_per_km'];?>"/>
+        
+                    <input type="text" class="login-input" name="city" placeholder="City" value="<?php echo $row['city'];?>"/>
+                
+                    <input type="submit" id="button" value="Update" class="login-button">
+                    <button style="width: 100%; height:50px; margin-top:10px; background-color:red; color:white; font-size:15px;">Cancel<a href="../profile.php"></a></buuton>
+            
+               </form>
+
+               
+            <?php } ?>
+            
+
+     
+  
+    </body>
+</html>
---
+++ driver/owner-vehicle-check.php
@@ -0,0 +1,57 @@
+<?php 
+ include "../db/connection.php";
+ session_start();
+
+ $targetDir = "../images/";
+ $fileName = basename($_FILES["file"]["name"]);
+ $targetFilePath = $targetDir . $fileName;
+ $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+
+ if (isset($_POST['title']) && isset($_POST['brand'])
+   && isset($_POST['city']) && isset($_POST['price']) && isset($_POST['id'])) {
+     function validate($data){
+         $data = trim($data);
+         $data = stripslashes($data);
+         $data = htmlspecialchars($data);
+         return $data;
+      }
+
+      $allowTypes = array('jpg','png','jpeg');
+      $title = validate($_POST['title']);
+      $brand = validate($_POST['brand']); 
+      $city = validate($_POST['city']);
+      $price= validate($_POST['price']);
+      $seat= validate($_POST['seats']);
+      $id=$_POST['id'];
+      //$avatar_path = $con->real_escape_string('../images/'.$_FILES['avatar']['name']);
+      $sql2="Select id from Brand where name='$brand';";
+      $result2 = mysqli_query($con,$sql2);
+      $rowb=mysqli_fetch_assoc($result2);
+      $brand_id=$rowb['id'];
+
+         //copy image to images/ folder 
+         if(in_array($fileType, $allowTypes)){
+           if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
+             $sql = "INSERT INTO vehicles(owner_id,vehicle_title,vehicles_brand,seats,price_per_km,city,vimage1) VALUES ('$id','$title', '$brand_id', '$seat','$price','$city','$fileName')";
+             $result = mysqli_query($con, $sql);      
+             if ($result){        
+                 header("Location: ../index.php");
+             }
+             else{
+                 header("Location: ./owner-vehicle.php");
+                 exit();
+             }
+           } 
+
+         }  
+
+
+ }
+ else{
+     header("Location: ./owner-vehicle.php");
+     exit();
+ }
+
+
+
+
