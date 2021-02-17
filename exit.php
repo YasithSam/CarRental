078b19ae65a1d57b4ddb83c0c410b7db5702cb55
YasithSam <yasith.sam7@gmail.com>
2021-02-17 22:37:25 +0530

    Exit file

---
+++ exit.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <title>Initial Page</title>
+    <link rel="stylesheet" href="./css/exit.css">
+</head>
+
+<body>
+
+<div class="header">
+    
+    <a href="home.php">Hire<span>Me</span></a>
+    <div class="header-right">
+    </div>
+</div>
+
+
+<div class="heading">
+  <div>
+    <p class="slide-up"><span class="red">Hello there,</span></p>
+  </div>
+  <div>
+    <p class="slide-up">Welcome to HireMe.</p>
+  </div>
+ 
+</div>
+<br /><br />
+
+<div class="q">
+    
+
+<!-- Sign in Buttons-->
+    <button type="button" onclick="location.href='./owner.php';" >Sign up to drive</button>&emsp;
+        &nbsp;<button type="button" onclick="location.href='./registration.php';" >Sign up to ride</button>
+        <div class="v_button">
+          <button type="button" onclick="location.href='./home.php';">Visitor</button>
+        </div>
+        
+</div>
+</body>
+
+</html>
